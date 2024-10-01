4ebb6ba10e3f9ce0cb600d5058dc5c476c0e35e7
Neha Asati <31asatineah@gmail.com>
2024-10-01 10:46:50 +0200

    add login in user

--- User.java
+++ User.java
@@ -1,2 +1,29 @@
-public class User {
+public abstract class User {
+
+    private String username;
+    private String password;
+
+
+    public User(String username, String password){
+        this.username =username;
+        this.password =password;
+    }
+
+    public abstract  boolean login(String username, String password);
+
+    public abstract void menu();
+
+   protected  String getUsername(){
+       return username;
+    }
+
+
+    protected String getPassword() {
+        return password;
+    }
+
+    protected void setPassword(String password) {
+        this.password = password;
+    }
 }
+
