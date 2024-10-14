1578621bdc89eee6579384ea00131e03170ff0d5
Neha Asati <31asatineah@gmail.com>
2024-10-14 16:47:20 +0200

    add function to student

--- Student.java
+++ Student.java
@@ -12,9 +12,24 @@ public Student(String username,String password){
     public void menu() {
         System.out.println("student menu");
         System.out.println("1.view information");
-        System.out.println("2.give exam");
+        System.out.println("2.write exam");
+        System.out.println("3.Write quiz");
+        System.out.println("4.get rating");
         System.out.println("3.Logout");
 
     }
+    public void viewInformation(){
+        System.out.println("Student"+getUsername()+"see all information view");
+    }
 
+    public void writeExam(){
+        System.out.println("Student"+getUsername()+"is giving exam");
+    }
+    public void writeQuiz(){
+        System.out.println("Student"+getUsername()+"is write quiz");
+    }
+    public void getRating()
+    {
+        System.out.println("Student"+getUsername()+"is get rating");
+    }
 }
